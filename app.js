961c5e1bfc0040801110c8144160d2b6b23a5175
Veronicachase <chase.veronica@gmail.com>
2024-10-28 15:32:56 +0100

    jenki7

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(cors())
 
 
 app.get('/', (req, res) => {
-    res.status(200).json({ message: 'Probando jenkins 3!' });
+    res.status(200).json({ message: 'Probando jenkins 4!' });
 });
 
 app.post('/echo', (req, res) => {
